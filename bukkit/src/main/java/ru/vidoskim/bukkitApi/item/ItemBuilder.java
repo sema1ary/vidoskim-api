4a1f08af47038860b1b9b54d933bab015f222e80
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-01-05 22:08:39 +0300

    Init

--- bukkit/src/main/java/ru/vidoskim/bukkitApi/item/ItemBuilder.java
+++ bukkit/src/main/java/ru/vidoskim/bukkitApi/item/ItemBuilder.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+@SuppressWarnings("unused")
 public class ItemBuilder {
     private String headId;
     private Material material;
