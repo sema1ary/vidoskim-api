e6a1ab423f6ed5a6c80b3aede89088b02a7e2c01
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-01-07 23:21:36 +0300

    Init

--- bukkit/src/main/java/ru/vidoskim/bukkitApi/BukkitApi.java
+++ bukkit/src/main/java/ru/vidoskim/bukkitApi/BukkitApi.java
@@ -3,6 +3,7 @@ package ru.vidoskim.bukkitApi;
 import lombok.Getter;
 import me.arcaniax.hdb.api.HeadDatabaseAPI;
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.vidoskim.bukkitApi.item.ItemListener;
 
 @Getter
 public final class BukkitApi extends JavaPlugin {
@@ -13,5 +14,7 @@ public final class BukkitApi extends JavaPlugin {
         if(getServer().getPluginManager().isPluginEnabled("HeadDatabase")) {
             headDatabaseAPI = new HeadDatabaseAPI();
         }
+
+        getServer().getPluginManager().registerEvents(new ItemListener(), this);
     }
 }
