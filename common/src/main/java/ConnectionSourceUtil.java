6b5e585701e1c8b26b05a46c53dd9848316a617d
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-01-08 13:20:28 +0300

    Терь код норм вроде

--- common/src/main/java/ConnectionSourceUtil.java
+++ common/src/main/java/ConnectionSourceUtil.java
@@ -2,23 +2,24 @@ import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
 
 @Getter
+@UtilityClass
 @SuppressWarnings("unused")
 public class ConnectionSourceUtil {
-    private JdbcPooledConnectionSource connectionSource;
-
     @SneakyThrows
-    public void connect(@NonNull String host, @NonNull String database, @NonNull String user, @NonNull String pass) {
-        connectionSource = new JdbcPooledConnectionSource(
+    public JdbcPooledConnectionSource connect(@NonNull String host, @NonNull String database, @NonNull String user, @NonNull String pass) {
+        JdbcPooledConnectionSource connectionSource = new JdbcPooledConnectionSource(
                 "jdbc:mysql://" + host + "/" + database + "?useSSL=true&autoReconnect=true");
         connectionSource.setUsername(user);
         connectionSource.setPassword(pass);
         connectionSource.setMaxConnectionsFree(5);
+        return connectionSource;
     }
 
     @SneakyThrows
-    public void closeConnection() {
+    public void closeConnection(JdbcPooledConnectionSource connectionSource) {
         connectionSource.close();
     }
 }
