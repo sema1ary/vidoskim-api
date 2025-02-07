da46cd31108b71bf86bce9dea114c36887f10586
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-02-07 15:39:28 +0300

    Добавил поддержку Sqlite, перенес dao

--- common/src/main/java/ormlite/ConnectionSourceUtil.java
+++ common/src/main/java/ormlite/ConnectionSourceUtil.java
@@ -1,7 +1,5 @@
 package ormlite;
 
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import lombok.NonNull;
 import lombok.SneakyThrows;
@@ -11,26 +9,36 @@ import lombok.experimental.UtilityClass;
 @SuppressWarnings("unused")
 public class ConnectionSourceUtil {
     @SneakyThrows
-    public JdbcPooledConnectionSource connect(String host, String database, String user, String pass, @NonNull Class<?>... modelClasses) {
-
+    public JdbcPooledConnectionSource connectSqlite(@NonNull String filePath) {
         JdbcPooledConnectionSource connectionSource = new JdbcPooledConnectionSource(
-           "jdbc:mysql://" + host + "/" + database + "?useSSL=true&autoReconnect=true");
-        connectionSource.setUsername(user);
-        connectionSource.setPassword(pass);
-        connectionSource.setMaxConnectionsFree(5);
-
-        TableCreatorUtil.create(connectionSource, modelClasses);
-        DaoCreatorUtil.create(connectionSource, modelClasses);
-
+                "jdbc:sqlite:" + filePath);
+        createModelDaoAndTable(connectionSource);
         return connectionSource;
     }
 
     @SneakyThrows
-    public void closeConnection(JdbcPooledConnectionSource connectionSource) {
-        connectionSource.close();
+    public JdbcPooledConnectionSource connectMySQL(String host, String database, String user, String pass, @NonNull Class<?>... modelClasses) {
+        JdbcPooledConnectionSource connectionSource = new JdbcPooledConnectionSource(
+                "jdbc:mysql://" + host + "/" + database + "?useSSL=true&autoReconnect=true");
+        setUpTheConnection(connectionSource, user, pass);
+        createModelDaoAndTable(connectionSource);
+
+        return connectionSource;
     }
 
-    public <D extends Dao<T, ?>, T> D getDao(JdbcPooledConnectionSource connectionSource, Class<T> daoClass) {
-        return DaoManager.lookupDao(connectionSource, daoClass);
+    private void setUpTheConnection(@NonNull JdbcPooledConnectionSource connectionSource, String user, String pass) {
+        connectionSource.setUsername(user);
+        connectionSource.setPassword(pass);
+        connectionSource.setMaxConnectionsFree(5);
+    }
+
+    private void createModelDaoAndTable(@NonNull JdbcPooledConnectionSource connectionSource, @NonNull Class<?>... modelClasses) {
+        TableCreatorUtil.create(connectionSource, modelClasses);
+        DaoUtil.create(connectionSource, modelClasses);
+    }
+
+    @SneakyThrows
+    public void closeConnection(JdbcPooledConnectionSource connectionSource) {
+        if(connectionSource != null) connectionSource.close();
     }
 }
--- common/src/main/java/ormlite/DaoUtil.java
+++ common/src/main/java/ormlite/DaoUtil.java
@@ -1,5 +1,6 @@
 package ormlite;
 
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import lombok.NonNull;
@@ -8,11 +9,15 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 @SuppressWarnings("unused")
-public class DaoCreatorUtil {
+public class DaoUtil {
     @SneakyThrows
     void create(@NonNull JdbcPooledConnectionSource connectionSource, @NonNull Class<?>... modelClasses) {
         for(Class<?> modelClass: modelClasses) {
             DaoManager.createDao(connectionSource, modelClass);
         }
+    }
+
+    public <D extends Dao<T, ?>, T> D getDao(JdbcPooledConnectionSource connectionSource, Class<T> daoClass) {
+        return DaoManager.lookupDao(connectionSource, daoClass);
     }
 }
