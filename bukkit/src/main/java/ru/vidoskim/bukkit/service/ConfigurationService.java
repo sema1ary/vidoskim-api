b77759fc258b9490aad5f4354a7ce97cf8ced4e5
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-02 00:39:59 +0300

    ConfigurationService

--- bukkit/src/main/java/ru/vidoskim/bukkit/service/ConfigurationService.java
+++ bukkit/src/main/java/ru/vidoskim/bukkit/service/ConfigurationService.java
@@ -1,9 +1,11 @@
 package ru.vidoskim.bukkit.service;
 
+import jdk.jfr.Experimental;
 import lombok.NonNull;
 import org.bukkit.plugin.Plugin;
 import service.Service;
 
+@Experimental
 @SuppressWarnings("unused")
 public interface ConfigurationService extends Service {
     void reload(@NonNull Plugin plugin);
