a60411cf38a922fee300e74427ae5b0f1f2f31f5
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-08 14:31:09 +0300

    DaoFinder

---
+++ common/src/main/java/ormlite/DaoFinder.java
@@ -0,0 +1,13 @@
+package ormlite;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+
+@SuppressWarnings("unused")
+public interface DaoFinder {
+    default <D extends Dao<T, ?>, T> D getDao(
+            JdbcPooledConnectionSource connectionSource, Class<T> daoClass) {
+        return DaoManager.lookupDao(connectionSource, daoClass);
+    }
+}
